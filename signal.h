c3f80b27786bc1842a796300d3b129aebb04a45a
Roberto Cruz <rec129@command.cs.rutgers.edu>
2016-03-07 16:39:01 -0500

    added signum siginfo and sighandler stuff in signal.h

--- signal.h
+++ signal.h
@@ -1,6 +1,10 @@
 #ifndef XV6_SIGNAL
 #define XV6_SIGNAL
+typedef void (sighandler_t)(siginfo_t) sighandler_t;
 
+struct {
+  int signum;
+} siginfo_t;
 // You should define anything signal related that needs to be shared between
 // kernel and userspace here
 
